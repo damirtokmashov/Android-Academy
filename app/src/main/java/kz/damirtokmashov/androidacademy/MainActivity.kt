0b427052204e68b079d9ee4a27c893aa683c93f5
Damir Tokmashov <damir.tokmashov@gmail.com>
2020-12-26 22:49:48 +0600

    Захардкодил переход во фрагменты

--- app/src/main/java/kz/damirtokmashov/androidacademy/MainActivity.kt
+++ app/src/main/java/kz/damirtokmashov/androidacademy/MainActivity.kt
@@ -1,11 +1,23 @@
 package kz.damirtokmashov.androidacademy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.Intents.Insert.DATA
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragment : MoviesListFragment = MoviesListFragment.newInstance()
+
+        // check is important to prevent activity from attaching the fragment if already its attached
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment, "fragment_name")
+                .commit()
+        }
     }
 }
